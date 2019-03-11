39f43e07ab1ad1331d5499960bee1379c1c25323
Boris Skurikhin <bskurikh@fred.socs.uoguelph.ca>
2019-03-11 15:52:36 -0400

    moved to materialize css

--- assign3/public/index.js
+++ assign3/public/index.js
@@ -1,7 +1,10 @@
 function init() {
   /* This function will get called on init */
-
+  var elem = document.querySelector('.tabs'); 
+  var instance = M.Tabs.init(elem, {});
 }
+
+
 
 $.ajax({
   type: "GET",
@@ -9,31 +12,18 @@ $.ajax({
   dataType: "JSON",
   success: function(res) {
     $('#numFiles').text(res['numFiles']);
-
-    var insert = '<ul class="list-group flex-sm-row">';
+    if (res['numFiles'] == 0 ) {
+      $('#statusMessage').append('There are <span class="pink-text">NO</span> files on the server.');
+    } else {
+      $('#statusMessage').append('There are <span class="pink-text">' + res['numFiles'] + '</span> files on the server.');
+    }
     res['files'].forEach(obj => {
-      insert += '<li class="list-group-item list-group-item-dark">';
-      insert += '<strong>Filename: </strong><p>' + obj['filename'] + '</p>';
-      insert += '</li>';
-
-      insert += '<li class="list-group-item list-group-item-dark">';
-      insert += '<strong>Version: </strong><p>' + obj['version'] + '</p>';
-      insert += '</li>';
-
-      insert += '<li class="list-group-item list-group-item-dark">';
-      insert += '<strong>Prod ID: </strong><p>' + obj['prodID'] + '</p>';
-      insert += '</li>';
-
-      insert += '<li class="list-group-item list-group-item-dark">';
-      insert += '<strong>Num Props: </strong><p>' + obj['numProps'] + '</p>';
-      insert += '</li>';
-
-      insert += '<li class="list-group-item list-group-item-dark">';
-      insert += '<strong>Num Events: </strong><p>' + obj['numEvents'] + '</p>';
-      insert += '</li>';
-
+      // <li class="tab"><a href="#test4">Test 1</a></li>
+      $('#tabList').append('<li class="tab"><a href="#' + obj['filename'] + '">' + obj['filename'] + '</a></li>');
+      // <div id="test4">Test 1</div>
+      var table = '<div id="' + obj['filename'] + '"><table><thead><tr><th>File Name</th><th>Version</th><th>Product ID</th><th># Events</th><th># Properties</th></tr></thead><tbody><tr><td>' + obj['filename'] + '</td><td>' + obj['version'] + '</td><td>' + obj['prodID'] + '</td><td>' + obj['numEvents'] + '</td><td>' + obj['numProps'] + '</td></tr></tbody></table></div>';
+      $('#tabVals').append(table);
     });
-    insert += '</ul>';
-    $('#insertCalendars').append(insert);
+    init();
   }
-})
+});
